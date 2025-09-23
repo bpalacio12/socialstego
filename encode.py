a2decf65c6f45d47f475baf87cef13d07250f367
Branden Palacio <134810377+bpalacio12@users.noreply.github.com>
2025-09-22 23:06:16 -0500

    select seperate files

--- encode.py
+++ encode.py
@@ -1,20 +1,23 @@
 import base64
 import tkinter as tk
 from tkinter import filedialog
+import os
+import numpy as np
 
-def select_files():
+
+def select_file(title):
     root = tk.Tk()
     root.withdraw()  # Hide the root window
-    file_paths = filedialog.askopenfilenames(title="Select files to encode")
-    return list(file_paths)
-
+    return filedialog.askopenfilenames(initialdir=os.path.expanduser("~/Documents"),title=title)
+    
 def main():
-    files = select_files()
-    for file_path in files:
-        with open(file_path, "rb") as f:
-            file_data = f.read()
-            encoded_data = base64.b64encode(file_data).decode("utf-8")
-            print(f"Encoded {file_path}: {encoded_data}")
+    target_file = select_file("target file")
+    sensitive_info = select_file("sensitive information")
+    # for file_path in files:
+    #     with open(file_path, "rb") as f:
+    #         file_data = f.read()
+    #         encoded_data = base64.b64encode(file_data).decode("utf-8")
+    #         print(f"Encoded {file_path}: {encoded_data}")
 
     return
 
