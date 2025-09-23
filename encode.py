2ea783efd81454102a3281bae038f42d3296d6ad
Branden Palacio <134810377+bpalacio12@users.noreply.github.com>
2025-09-22 23:23:55 -0500

    venv is working

--- encode.py
+++ encode.py
@@ -3,13 +3,24 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 import numpy as np
-
+from PIL import Image
 
 def select_file(title):
     root = tk.Tk()
     root.withdraw()  # Hide the root window
     return filedialog.askopenfilenames(initialdir=os.path.expanduser("~/Documents"),title=title)
     
+def encode(src,data,dst):
+    img=Image.open(src, 'r')
+    width,height=img.size
+    array=np.array(list(img.getdata()))
+
+    if img.mode=='RGB':
+        n=3
+    elif img.mode=='RGBA':
+        n=4
+    total_pixels=array.size//n
+
 def main():
     target_file = select_file("target file")
     sensitive_info = select_file("sensitive information")
