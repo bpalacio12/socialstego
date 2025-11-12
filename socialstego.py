a182efc3c42f51d1e4d2678bef909d5317431e59
Branden Palacio <134810377+bpalacio12@users.noreply.github.com>
2025-11-11 18:02:21 -0600

    working trial for encoding png and extracting file type based on magic bytes

--- socialstego.py
+++ socialstego.py
@@ -5,6 +5,14 @@ import numpy as np
 import argparse
 from PIL import Image
 
+# mapping for file signatures
+magic_numbers = {
+    "pdf": bytes.fromhex("255044462d"),
+    "png": bytes.fromhex("89504e470d0a1a0a"),
+    "gzip": bytes.fromhex("1f8b")
+}
+
+# Argument parsing to determine either encoding or decoding actions
 def parse_args():
     parser=argparse.ArgumentParser(description="Stegonagraphy Encoder/Decoder script")
     group = parser.add_mutually_exclusive_group(required=True)
@@ -18,7 +26,7 @@ def select_file(title):
     root.withdraw()  # Hide the root window
     return filedialog.askopenfilename(initialdir=os.path.expanduser("~/Documents"),title=title)
 
-def file_to_bytes(file_path):
+def file_to_bits(file_path):
     with open(file_path, "rb") as f:
         for byte in f.read():
             for i in range(7,-1,-1):
@@ -38,7 +46,7 @@ def encode():
         exit(1)
     
     dst=os.path.join(os.path.dirname(os.path.abspath(__file__)),"encoded.png")
-    data=file_to_bytes(sensitive_info)
+    data=file_to_bits(sensitive_info)
 
     img=Image.open(src, 'r')
     width,height=img.size
@@ -53,7 +61,12 @@ def encode():
     
     total_pixels=array.size//n
 
-    bit_gen= (bit for bit in list(data)+list(marker_bits()))
+    # bit_gen= (bit for bit in list(data)+list(marker_bits()))s
+    # size=len(list(bit_gen))
+
+    data_bits=list(data)+list(marker_bits())
+    size=len(data_bits)
+    bit_gen= iter(data_bits)
 
     for p in range(total_pixels):
         for q in range(0,3):
@@ -66,6 +79,7 @@ def encode():
     enc_img = Image.fromarray(array.astype('uint8'), img.mode)
     enc_img.save(dst)
     print("Image successfully encoded")
+    print((int)(size/8),"bytes encoded")
 
 def decode():
     src = select_file("File to decode")    
@@ -95,10 +109,21 @@ def decode():
     if marker_index!=-1:
         data_bytes=data_bytes[:marker_index]
 
+    magic=extract_magic(data_bytes[:10]) # calls extract_magic to determine the recovered file type
+    dst= dst+"."+magic
+
     with open(dst, "wb") as f:
         f.write(data_bytes)
 
     print(f"file successfully reconstructed as {dst}")
+
+# will return the embedded file type based on magic bytes, returns .txt if no matching header found
+def extract_magic(header):
+    print(header)
+    for filetype, signature in magic_numbers.items():
+        if header.startswith(signature):
+            return filetype
+    return "txt"
 
 # we are going to start by assuming the target file is a PNG then move forward with other media types 
 def main():
